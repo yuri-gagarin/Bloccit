42c09f63ba2ae094a6e36dfd0c508847847e1a6c
Yuriy Ivanov <ymivanov@icloud.com>
2016-11-24 02:01:45 -0500

    19-RailsCRUD Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,29 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all 
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+        flash[:notice] = "Post was saved."
+        redirect_to @post
+      else
+        flash.now[:alert] = "There was an error  saving the post. Please try again."
+        render :new
+    end
   end
 
   def edit
