cbe50467bf87453da390bf16c2b22adccfb8edc3
Yuriy Ivanov <ymivanov@icloud.com>
2016-12-19 02:37:05 -0500

    Error corrected

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214084749) do
+ActiveRecord::Schema.define(version: 20161219070512) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20161214084749) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
